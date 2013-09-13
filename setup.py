bbf071e8be678d243436a54bcfbc03f59930d5fb
chebee7i <chebee7i@gmail.com>
2013-09-13 02:33:48 -0500

    Install using setuptools with dependency resolution.

--- setup.py
+++ setup.py
@@ -9,13 +9,18 @@ from __future__ import print_function
 import os
 import sys
 
-from distutils.core import setup
+try:
+    from setuptools import setup
+    has_setuptools = True
+except ImportError:
+    from distutils.core import setup
+    has_setuptools = False
 
 def main():
 
-    requires = [
-        'networkx(>=1.6)',
-        'pyparsing(>=2.0.1)',
+    install_requires = [
+        'networkx >= 1.6',
+        'pyparsing >= 2.0.1',
     ]
 
     packages = [
@@ -28,21 +33,28 @@ def main():
 and `graphviz`. Support is also provided for inline displays within IPython
 notebooks.
 """
-    setup(
-          name             = "nxpd",
-          version          = "0.1dev",
-          url              = "https://github.com/chebee7i/nxpd",
 
-          packages         = packages,
-          provides         = ['nxpd'],
-          requires         = requires,
+    kwds = {
+        'name':              "nxpd",
+        'version':           "0.1dev",
+        'url':               "https://github.com/chebee7i/nxpd",
 
-          author           = "chebee7i",
-          author_email     = "chebee7i@gmail.com",
-          description      = "NetworkX Pydot Draw",
-          long_description = description,
-          license          = "Unlicense",
-         )
+        'packages':          packages,
+        'provides':          ['nxpd'],
+        'install_requires':  install_requires,
+
+        'author':            "chebee7i",
+        'author_email':      "chebee7i@gmail.com",
+        'description':       "NetworkX Pydot Draw",
+        'long_description':  description,
+        'license':           "Unlicense",
+    }
+
+    # Automatic dependency resolution is supported only by setuptools.
+    if not has_setuptools:
+        del kwds['install_requires']
+
+    setup(**kwds)
 
 if __name__ == '__main__':
 
