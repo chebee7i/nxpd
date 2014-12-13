4e278a0ed66dd7a7e2ba3bfb94dd6c393128a1b4
chebee7i <chebee7i@gmail.com>
2014-12-12 21:14:31 -0600

    Bugfix release, already: 0.1.1

--- nxpd/__init__.py
+++ nxpd/__init__.py
@@ -5,6 +5,6 @@ from __future__ import absolute_import
 from .nx_pydot import draw_pydot as draw
 from .params import nxpdParams
 
-__version__ = '0.1'
+__version__ = '0.1.1'
 
 __all__ = ['draw', 'nxpdParams']
--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ notebooks.
 
     kwds = {
         'name':              "nxpd",
-        'version':           "0.1",
+        'version':           "0.1.1",
         'url':               "https://github.com/chebee7i/nxpd",
 
         'packages':          packages,
